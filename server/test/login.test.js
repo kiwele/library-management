de43cbe7dcd04d49b3a7849bb1c0fbf43bceab65
kiwele <musakiwele@gmail.com>
2023-01-23 18:52:32 +0300

    initial commit

--- server/test/login.test.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/extensions */
-import jest from 'jest';
-// import ClientCtrl from '../controllers/client.js';
-import ClientCtrl from '../controllers/client.js';
-
-test('User inputs', () => {
-  const email = 'musa@gmail.com';
-  expect(email).toMatch(/gmail/);
-});
